969685050a1ebded1b97ee235e8bc27ac6f02e70
lobovalexey6 <lobovalexey6@yandex.ru>
2024-04-19 22:03:41 +0500

    elschool functions

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from data import db_session
 
 if __name__ == '__main__':
     db_session.global_init("db/users.db")
-    db_session.add_user('Lobov_Aleksej4', 'Alexey67', '6', 'aidi')
-    print(db_session.get_user('aidi', '6'))
+
 
